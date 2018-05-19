8c3256a83b1afb8ba7ca4e14e35ede32fe8e7f33
高辛格 <xg.gao@tianrang-inc.com>
2018-05-19 12:25:07 +0800

    c

---
+++ c/ooc/004/new.h
@@ -0,0 +1,10 @@
+#ifndef NEW_H
+#define NEW_H
+
+void*  new(const void* _class, ...);
+void   delete(void* self);
+void*  clone(const void* self);
+int    differ(const void* self, const void* b);
+size_t sizeOf(const void* self);
+
+#endif
---
+++ c/ooc/004/new.r
@@ -0,0 +1,7 @@
+struct Class {
+    size_t size;
+    void* (* ctor)   (void* self, va_list* app);
+    void* (* dtor)   (void* self);
+    void* (* clone)  (const void* self);
+    int   (* differ) (const void* self, const void* b);
+};
