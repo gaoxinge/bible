b95c1d38de74ee57a722f781491ca78e1dfd70be
高辛格 <xg.gao@tianrang-inc.com>
2018-07-20 18:01:42 +0800

    asynchronous

---
+++ asynchronous/Python黑魔法之Generator Coroutines/tornado1/t.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from tornado.gen import coroutine, moment
+from tornado.ioloop import IOLoop
+
+
+@coroutine
+def task1():
+    print("task1", 1)
+    yield moment
+    print("task1", 2)
+
+
+@coroutine
+def task2():
+    print("task2", 1)
+    yield moment
+    print("task2", 2)
+
+
+if __name__ == "__main__":
+    t1 = task1()
+    t2 = task2()
+    IOLoop.current().start()
